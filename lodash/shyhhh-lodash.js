e426b2cac31bb84a6a2720f6c378886d340dd3fe
何宏贺 <hhhtheshy@gmail.com>
2022-09-14 18:11:05 +0800

    chunk ok

--- lodash/shyhhh-lodash.js
+++ lodash/shyhhh-lodash.js
@@ -2,14 +2,15 @@ var shyhhh = {
   chunk: function (array, size = 1) {
     let res = []
     for (let i = 0; i < array.length; i++) {
-      let group = []
-      for (let j = 0; j < size; j++) {
-        group.push(array[i++])
-        if (i === array.length) {
+      let temp = []
+      for (let j = size; j > 0; j--) {
+        temp.push(array[i])
+        if (j > size - 1) i++
+        if (i == array.length) {
           break
         }
       }
-      res.push(group)
+      res.push(temp)
     }
     return res
   }
