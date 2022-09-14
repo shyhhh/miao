42030431e0b7fd593198c46cd0faab17dcff2572
何宏贺 <hhhtheshy@gmail.com>
2022-09-14 19:52:08 +0800

    1

--- lodash/shyhhh-lodash.js
+++ lodash/shyhhh-lodash.js
@@ -1,15 +1,28 @@
 var shyhhh = {
   chunk: function (array, size = 1) {
-   return Array.from({
+    return Array.from({
         length: Math.ceil(array.length / size)
       }, (v, i) =>
       array.slice(i * size, i * size + size)
     )
   },
   compact: function (array) {
-    return arr.filter(Boolean)
+    return array.filter(Boolean)
   },
   concat: function (array, ...value) {
     return array.concat(...value)
+  },
+  difference: function (array, ...value) {
+    const s = new Set(...value)
+    return array.filter(x => !s.has(x))
+  },
+  differenceBy: function (array, [values], [iteratee = _.identity]) {
+
+  },
+  differenceWith: function (array, val, comp) {
+   return array.filter(a => val.findIndex(b => comp(a, b)) === -1)
+  },
+  drop: function (array, n = 1) {
+    return array.splice(n)
   }
 }
