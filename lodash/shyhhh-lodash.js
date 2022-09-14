305b42b83038f100448fdbc7674546c8db9fa46f
何宏贺 <hhhtheshy@gmail.com>
2022-09-14 17:22:26 +0800

    add chunk

--- lodash/shyhhh-lodash.js
+++ lodash/shyhhh-lodash.js
@@ -1,6 +1,16 @@
 var shyhhh = {
-  chunk(arr, n) {
-
+  chunk(arr, n = 1) {
+    let arr = []
+    for (let i = 0; i < arr.length; i++) {
+      let group = []
+      for (let j = 0; j < n; j++) {
+        group.push(arr[i++])
+        if (i === arr.length) {
+          break
+        }
+      }
+      arr.push(group)
+    }
+    return arr
   }
-
 }
